3723c96352acbab386d96d1a5ed1554ace1a7c3b
JiffyLTD <gosharodionov3@mail.ru>
2023-10-06 01:11:22 +0300

    добавлено версионирование апи

--- ITHub.Notes.API/Contollers/NoteController.cs
+++ ITHub.Notes.API/Contollers/NoteController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ITHub.Notes.API.Contollers
 {
-    [Route("api/1")]
+    [Route("api/v1")]
     [ApiController]
     public class NoteController : ControllerBase
     {
